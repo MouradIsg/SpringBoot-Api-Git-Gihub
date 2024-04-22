bdf84cf2593f3df9751de1cb0aae9bdb9060c090
mourad <mourad.agenuctech@gmail.com>
2024-04-22 22:41:51 +0000

    Implementing dao Layer

--- src/main/java/com/mourad/SpringApiStudents/student/StudentService.java
+++ src/main/java/com/mourad/SpringApiStudents/student/StudentService.java
@@ -5,27 +5,12 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.List;
 
-@Service
-public class StudentService {
 
-    public List<Student> findAllStudents(){
-        return List.of(
-                new Student(
-                        "Mourad",
-                        "Gharsalli",
-                        LocalDate.now(),
-                        "mourad.gh@gmail.com",
-                        38
+public interface StudentService {
+    Student save(Student s);
+    List<Student> findAllStudents();
+    Student findByEmail(String email);
+    Student update (Student s );
+    void delete( String email);
 
-                ),
-                new Student(
-                        "aliiiii33333333",
-                        "Gharsalli",
-                        LocalDate.now(),
-                        "ahmed.gh@gmail.com",
-                        12
-
-                )
-        );
-    }
 }
